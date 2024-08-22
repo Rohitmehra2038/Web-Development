75b0c7f0528fa0848ceccabecebaa2e52d70f09b
Rohit Mehra <142257980+Rohitmehra2038@users.noreply.github.com>
2024-08-22 14:39:34 +0530

    Add files via upload

---
+++ Cricket Website/script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
