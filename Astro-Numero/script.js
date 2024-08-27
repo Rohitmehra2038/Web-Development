e4c0e4c173f78f8cdfa3ae120dc16434f8359536
Rohit Mehra <142257980+Rohitmehra2038@users.noreply.github.com>
2024-08-27 19:54:56 +0530

    Add files via upload

---
+++ Astro-Numero/script.js
@@ -0,0 +1,11 @@
+// You can add any interactivity you want here
+// For example, smooth scrolling to sections on click
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
